9b5ba5695dc6cdc546ef37d6bf569468a058b55e
Limhyungwoo <rucy977@gmail.com>
2023-05-28 12:11:41 +0000

    Why

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> "DC-ON is very very very good"
+          Edit <code>src/App.js</code> "Why So tlfldntm"
         </p>
         <a
           className="App-link"
