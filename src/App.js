1f9d6a6c578fdda64873eabf683e2caa5e13affe
Limhyungwoo <rucy977@gmail.com>
2023-05-28 12:14:50 +0000

    money

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> "Why So tlfldntm"
+          Edit <code>src/App.js</code> "Your momey is mine"
         </p>
         <a
           className="App-link"
