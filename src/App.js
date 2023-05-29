f1f7ee852271b30595fe44ee7f513eab5ffa5966
Limhyungwoo <rucy977@gmail.com>
2023-05-29 23:49:12 +0000

    Today

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> "Your momey is mine"
+          Edit <code>src/App.js</code> "Today is 2023-05-30 8:48 AM"
         </p>
         <a
           className="App-link"
